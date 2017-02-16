593be5a976d66dd6be41bd820de83e4d9aa39faa
xsnpdngv <dezso.t.tamas@gmail.com>
2017-02-16 15:19:30 +0100

    Update root.sh

--- root.sh
+++ root.sh
@@ -5,7 +5,7 @@ echo "* Distribute the cacert.pem to clients to authenticate your server"
 echo "* Enter a (new) secret pass phrase when requested"
 echo "* Enter it again when prompted"
 echo "* You need the pass phrase later to sign the client and server key files"
-echo "* Enter your company name as the Common Name (e.g. genivia.com) when requested"
+echo "* Enter your company name as the Common Name when requested"
 echo "* The root CA will expire after three years (1095 days)"
 
 openssl req -newkey rsa:1024 -sha1 -keyout rootkey.pem -out rootreq.pem
